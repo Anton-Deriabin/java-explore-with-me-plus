9db0a5a2d86ce97005fa495876a7664348e33141
marloq1 <yoguar0802@yandex.ru>
2025-03-23 17:05:25 +0300

    Добавлена проверка времени в stats

--- stats/stats-server/src/main/java/ru/practicum/server/StatsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/server/StatsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.StatsDto;
+import ru.practicum.exception.ValidationException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -33,15 +34,18 @@ public class StatsServiceImpl implements StatsService {
     public List<StatsDto> getStats(String start, String end, List<String> uris, boolean unique) {
         LocalDateTime startTime = LocalDateTime.parse(start, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         LocalDateTime endTime = LocalDateTime.parse(end, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        List<StatsDto> stats = statsRepository.findStats(startTime, endTime, uris);
+        if (startTime.isAfter(endTime)) {
+            throw new ValidationException("Событие не может заканчиваться позже, чем начинается");
+        }
+        List<StatsDto> stats;
         if (unique) {
-            stats = stats.stream()
-                    .map(stat -> {
-                        long uniqueHits = statsRepository.countDistinctByAppAndUriAndTimestampBetween(stat.getApp(),
-                                stat.getUri(), startTime, endTime);
-                        return new StatsDto(stat.getApp(), stat.getUri(), uniqueHits);
-                    })
-                    .collect(Collectors.toList());
+            stats = statsRepository.findStatsWithUniqueIp(startTime, endTime, uris);
+            stats.forEach(stat -> log.info("Уникальные IP для app={}, uri={}: {}",
+                    stat.getApp(), stat.getUri(), stat.getHits()));
+        } else {
+            stats = statsRepository.findStats(startTime, endTime, uris);
+            stats.forEach(stat -> log.info("Все просмотры для app={}, uri={}: {}",
+                    stat.getApp(), stat.getUri(), stat.getHits()));
         }
         return logAndReturn(stats, result -> log.info("Статистика собрана в количестве - {}",
                 result.size()));
